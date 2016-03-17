9a81a7f6b9e8c30aff8bba4a366811cea3d3584a
sylviataylor <Sylvia@Sylvias-Air.wv.cc.cmu.edu>
2016-03-17 19:53:11 -0400

    woo

---
+++ mywebsite/index.js
@@ -0,0 +1,38 @@
+
+
+
+
+function kosbie_face_event_handler() {
+    alert("ouch")
+}
+
+function on_doc_ready() {
+    $(".kosbiesface").click(kosbie_face_event_handler)
+}
+
+$(document).ready(on_doc_ready)
+
+
+
+
+
+
+// function myFunction(){
+//     var x = 4;
+//     var L = [1,2,3]
+//     var s = "hello"
+
+//     if (x<10) {
+
+//     } else if(x<20){
+
+//     } else{
+
+//     }
+//     while (x>0){
+
+//     }
+//     for (var i=0; i<10; i++) {
+
+//     }
+// }
